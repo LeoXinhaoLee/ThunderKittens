6d6f1e44132ea10c062e54d985be2dc30fac7588
LeoXinhaoLee <Mc1114207918@outlook.com>
2024-06-01 21:23:37 -0700

    Add M1 whole loop

--- examples/M1_loop_one_body/tk_m1_prefill.cu
+++ examples/M1_loop_one_body/tk_m1_prefill.cu
@@ -19,7 +19,7 @@ using namespace kittens;
 
 template <typename H, typename T>
 __global__
-void prefill_ker(
+void prefill_loop_body_ker(
         int CS, int HF,
         T* __W1,
         const T* __XA, const T* __XB, const T* __XC,
@@ -98,7 +98,7 @@ void prefill_ker(
 }
 
 void
-prefill
+prefill_loop_body
         (
                 torch::Tensor W1,
                 torch::Tensor XA,
@@ -118,7 +118,7 @@ prefill
 
     auto threads = workers * kittens::WARP_THREADS;
 
-    prefill_ker<H,T><<<batch_mul_head, threads, 0, stream>>>(
+    prefill_loop_body_ker<H,T><<<batch_mul_head, threads, 0, stream>>>(
             cs, hf,
             W1.data_ptr<T>(),
             XA.data_ptr<T>(), XB.data_ptr<T>(), XC.data_ptr<T>(),
@@ -127,3 +127,130 @@ prefill
 
     // CHECK_CUDA_ERROR(cudaDeviceSynchronize());
 }
+
+
+template <typename H, typename T>
+__global__
+void prefill_whole_loop_ker(
+        const int NC, const int CS, const int HF,
+        T* __W1,
+        const T* __XA, const T* __XB, const T* __XC,
+        T* __Output
+) {
+    H *_W1       = reinterpret_cast<H*>(__W1) + blockIdx.x * (HF*HF);
+    const H *_XA       = reinterpret_cast<const H*>(__XA) + blockIdx.x * (NC*CS*HF);
+    const H *_XB       = reinterpret_cast<const H*>(__XB) + blockIdx.x * (NC*CS*HF);
+    const H *_XC       = reinterpret_cast<const H*>(__XC) + blockIdx.x * (NC*CS*HF);
+    H *_Output = reinterpret_cast<H*>(__Output) + blockIdx.x * (NC*CS*HF);
+
+    rt_bf<4, 4, kittens::ducks::rt_layout::col> W1_reg;
+    rt_bf<1, 4> XA_reg;
+    rt_bf<1, 4> XB_reg;
+    rt_bf<1, 4> XC_reg;
+
+    rt_fl<1, 4> Z1_fl_reg;
+    rt_bf<1, 4> Z1_reg;
+
+    rt_bf<1, 4> Output_reg;
+    rt_fl<1, 4> Z1_bar_term_1_fl_reg;
+    rt_bf<1, 4> Z1_bar_term_1_reg;
+    rt_fl<1, 4> Z1_bar_term_2_fl_reg;
+    rt_bf<1, 4> Z1_bar_term_2_reg;
+    rt_fl<1, 1> Attn1_fl_reg;
+    rt_bf<1, 1> Attn1_reg;
+    rt_fl<4, 4> W1_fl_reg;
+    rt_bf<4, 4> W1_row_reg;
+
+    load(W1_reg, _W1, W1_reg.cols);
+
+    for (int i = 0; i < NC; i++) {
+
+        // rt_bf<1, 4> XB_reg;
+        load(XB_reg, _XB + i * CS * HF, XB_reg.cols);
+        // rt_fl<1, 4> Z1_fl_reg;
+        zero(Z1_fl_reg);
+        mma_AB(Z1_fl_reg, XB_reg, W1_reg, Z1_fl_reg); // [K,f] r, [f,f] c -> [K,f] r
+
+        // rt_bf<1, 4> XA_reg;
+        load(XA_reg, _XA + i * CS * HF, XA_reg.cols);
+
+        // rt_bf<1, 4> Z1_reg;
+        copy(Z1_reg, Z1_fl_reg);
+        sub(Z1_reg, Z1_reg, XA_reg);
+
+        rt_bf<1, 4, ducks::rt_layout::col> &Z1_col_reg = swap_layout_inplace(Z1_reg);
+
+        // rt_bf<1, 4> XC_reg;
+        load(XC_reg, _XC + i * CS * HF, XC_reg.cols);
+        // rt_fl<1, 1> Attn1_fl_reg;
+        zero(Attn1_fl_reg);
+        mma_ABt(Attn1_fl_reg, XC_reg, XB_reg, Attn1_fl_reg);
+
+        // rt_bf<1, 1> Attn1_reg;
+        copy(Attn1_reg, Attn1_fl_reg);
+        make_causal(Attn1_reg, Attn1_reg, base_types::constants<bf16>::zero());
+
+        // rt_fl<1, 4> Z1_bar_term_1_fl_reg;
+        zero(Z1_bar_term_1_fl_reg);
+        mma_AB(Z1_bar_term_1_fl_reg, XC_reg, W1_reg, Z1_bar_term_1_fl_reg); // [N,K] r, [K,M] c -> [N,M] r
+        // rt_bf<1, 4> Z1_bar_term_1_reg;
+        copy(Z1_bar_term_1_reg, Z1_bar_term_1_fl_reg);
+
+        // rt_fl<1, 4> Z1_bar_term_2_fl_reg;
+        zero(Z1_bar_term_2_fl_reg);
+        mma_AB(Z1_bar_term_2_fl_reg, Attn1_reg, Z1_col_reg, Z1_bar_term_2_fl_reg);  // [K,K] r, [K,f] c -> [K,f] r
+        // rt_bf<1, 4> Z1_bar_term_2_reg;
+        copy(Z1_bar_term_2_reg, Z1_bar_term_2_fl_reg);
+
+        // rt_bf<1, 4> Output_reg;
+        sub(Output_reg, Z1_bar_term_1_reg, Z1_bar_term_2_reg);
+        store(_Output + i * CS * HF, Output_reg, Output_reg.cols);
+        rt_bf<1, 4, kittens::ducks::rt_layout::col> &XB_col_reg = swap_layout_inplace(XB_reg);
+
+        // rt_fl<4, 4> W1_fl_reg;
+        zero(W1_fl_reg);
+        mma_AtB(W1_fl_reg, XB_col_reg, Z1_col_reg, W1_fl_reg);
+
+        // rt_bf<4, 4> W1_row_reg;
+        copy(W1_row_reg, W1_fl_reg);
+        rt_bf<4, 4, kittens::ducks::rt_layout::col> &W1_col_reg = swap_layout_inplace(W1_row_reg);
+
+        sub(W1_reg, W1_reg, W1_col_reg);
+    }
+
+    store(_W1, W1_reg, W1_reg.cols);
+}
+
+
+void
+prefill_whole_loop
+        (
+                torch::Tensor W1,
+                torch::Tensor XA,
+                torch::Tensor XB,
+                torch::Tensor XC,
+                torch::Tensor Output,
+                cudaStream_t stream
+        ) {
+    auto batch = XA.size(0);
+    auto head = XA.size(1);
+    auto NC = XA.size(2);
+    auto CS = XA.size(3);
+    auto HF = XA.size(4);
+
+    using H = __nv_bfloat16;
+    using T = c10::BFloat16;
+    const int workers = 1;
+
+    auto threads = workers * kittens::WARP_THREADS;
+
+    prefill_whole_loop_ker<H, T><<<batch * head, threads, 0, stream>>>(
+            NC, CS, HF,
+            W1.data_ptr<T>(),
+            XA.data_ptr<T>(), XB.data_ptr<T>(), XC.data_ptr<T>(),
+            Output.data_ptr<T>()
+    );
+
+
+}
+
--- examples/M1_loop_one_body/tk_m1_prefill_frontend.cpp
+++ examples/M1_loop_one_body/tk_m1_prefill_frontend.cpp
@@ -3,21 +3,36 @@
 #include <vector>
 
 
-extern void  prefill(torch::Tensor W1,
+extern void  prefill_loop_body(torch::Tensor W1,
                      torch::Tensor XA, torch::Tensor XB, torch::Tensor XC,
                      torch::Tensor Out,
                      cudaStream_t stream);
 
-extern void  prefill_ref(torch::Tensor W1,
+extern void  prefill_loop_body_ref(torch::Tensor W1,
                          torch::Tensor XA, torch::Tensor XB, torch::Tensor XC,
                          torch::Tensor Out)
 {
     auto stream = at::cuda::getCurrentCUDAStream();
-    prefill(W1, XA, XB, XC, Out, stream);
+    prefill_loop_body(W1, XA, XB, XC, Out, stream);
 }
+
+extern void  prefill_whole_loop(torch::Tensor W1,
+                               torch::Tensor XA, torch::Tensor XB, torch::Tensor XC,
+                               torch::Tensor Out,
+                               cudaStream_t stream);
+
+extern void  prefill_whole_loop_ref(torch::Tensor W1,
+                                   torch::Tensor XA, torch::Tensor XB, torch::Tensor XC,
+                                   torch::Tensor Out)
+{
+    auto stream = at::cuda::getCurrentCUDAStream();
+    prefill_whole_loop(W1, XA, XB, XC, Out, stream);
+}
+
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.doc() = "Test handler for warp test"; // optional module docstring
-    m.def("prefill", &prefill_ref);
+    m.def("prefill_loop_body", &prefill_loop_body_ref);
+    m.def("prefill_whole_loop", &prefill_whole_loop_ref);
 }
