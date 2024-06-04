f5d487369d53842bebad426efe056930e1a85106
LeoXinhaoLee <Mc1114207918@outlook.com>
2024-06-04 15:25:18 -0700

    All swap layout use in-place. Slower 158ms

--- examples/M2/tk_m2_prefill.cu
+++ examples/M2/tk_m2_prefill.cu
@@ -39,40 +39,41 @@ void prefill_whole_loop_ker(
     rt_bf<4, 16, kittens::ducks::rt_layout::col> W1_col_reg;
     rt_bf<16, 4, kittens::ducks::rt_layout::col> W2_col_reg;
 
-    load(W1_col_reg, _W1, W1_col_reg.cols); // 32KB
-    load(W2_col_reg, _W2, W2_col_reg.cols); // 32KB - 64KB
+    load(W1_col_reg, _W1, W1_col_reg.cols); // 32KB; 256R
+    load(W2_col_reg, _W2, W2_col_reg.cols); // 32KB - 64KB; 256R - 512R
 
     for (int i = 0; i < NC; i++) {
         // Forward
-        rt_fl<1, 16> Z1_fl_reg; // 16KB - 80KB
-        rt_bf<1, 16> Z1_reg; // 8KB - 88KB
-        rt_fl<1, 4> Z2_fl_reg; // 4KB - 92KB
-        rt_bf<1, 4> XB_reg; // 2KB - 94KB
+        rt_bf<1, 4> XB_reg; // 2KB - 94KB; 32R - 768R
+        rt_fl<1, 16> Z1_fl_reg; // 16KB - 80KB; 128R - 640R
+        rt_bf<1, 16> Z1_reg; // 8KB - 88KB; 64R - 704R
 
         load(XB_reg, _XB + i * X_STRIDE, XB_reg.cols);  // [K,f]
         zero(Z1_fl_reg);  // [K,4f]
         mma_AB(Z1_fl_reg, XB_reg, W1_col_reg, Z1_fl_reg); // [K,f]r, [f,4f]c -> [K,4f]r
         copy(Z1_reg, Z1_fl_reg); // 16KB - 78KB
+
+        rt_fl<1, 4> Z2_fl_reg; // 4KB - 92KB; 32R - 736R
         zero(Z2_fl_reg); // [K,f]
         mma_AB(Z2_fl_reg, Z1_reg, W2_col_reg, Z2_fl_reg); // [K,4f]r, [4f,f]c -> [K,f]r
 
         // dl_dZ2
-        rt_bf<1, 4> dl_dZ2_reg; // 2KB - 80KB
         rt_bf<1, 4> XA_reg; // 2KB - 82KB
+        rt_bf<1, 4> dl_dZ2_reg; // 2KB - 80KB
 
         load(XA_reg, _XA + i * X_STRIDE, XA_reg.cols);  // [K,f]
         copy(dl_dZ2_reg, Z2_fl_reg); // 4KB - 78KB
         sub(dl_dZ2_reg, dl_dZ2_reg, XA_reg);  // [K,f] // 2KB - 76KB
 
         // delta W2
-        rt_fl<16, 4> delta_W2_fl_reg; // 64KB - 140KB
-        rt_bf<16, 4> delta_W2_reg; // 32KB - 172KB
-        rt_bf<1, 16, ducks::rt_layout::col> Z1_col_reg; // 8KB - 180KB
-        swap_layout(Z1_col_reg, Z1_reg);
-        rt_bf<1, 4, ducks::rt_layout::col> dl_dZ2_col_reg; // 2KB - 182KB
-        swap_layout(dl_dZ2_col_reg, dl_dZ2_reg);  // cannot in-place swap dl_dZ21_reg since it will be needed later
+        rt_bf<1, 16, ducks::rt_layout::col> &Z1_col_reg = swap_layout_inplace(Z1_reg); // 8KB - 180KB
+        rt_bf<1, 4, ducks::rt_layout::col> &dl_dZ2_col_reg = swap_layout_inplace(dl_dZ2_reg);
+        rt_fl<16, 4> delta_W2_fl_reg;
+        rt_bf<16, 4> delta_W2_reg;
+
         zero(delta_W2_fl_reg);
         mma_AtB(delta_W2_fl_reg, Z1_col_reg, dl_dZ2_col_reg, delta_W2_fl_reg);  // ([K,4f]c).t @ [K,f]c -> [4f,f]r // 8KB - 174KB
+        swap_layout_inplace(Z1_col_reg);
         copy(delta_W2_reg, delta_W2_fl_reg); // 64KB - 110KB
         rt_bf<16, 4, ducks::rt_layout::col> &delta_W2_col_reg = swap_layout_inplace(delta_W2_reg);  // TODO: tricky
 
@@ -81,19 +82,20 @@ void prefill_whole_loop_ker(
         rt_fl<1, 16> dl_dZ1_fl_reg; // 16KB - 134KB
 
         zero(dl_dZ1_fl_reg);
-        rt_bf<16, 4, kittens::ducks::rt_layout::row> W2_reg; // 32KB - 166KB
-        swap_layout(W2_reg, W2_col_reg);
+        rt_bf<16, 4, kittens::ducks::rt_layout::row> &W2_reg = swap_layout_inplace(W2_col_reg);
+        swap_layout_inplace(dl_dZ2_col_reg);  // c->r
         mma_ABt(dl_dZ1_fl_reg, dl_dZ2_reg, W2_reg, dl_dZ1_fl_reg);  // [K,f]r @ [4f,f]r.t -> [K,4f]r // 32KB+2KB - 132KB
+        swap_layout_inplace(W2_reg);
         copy(dl_dZ1_reg, dl_dZ1_fl_reg); // 16KB - 116KB
 
         // delta W1
         rt_fl<4, 16> delta_W1_fl_reg; // 64KB - 180KB
         rt_bf<4, 16> delta_W1_reg; // 32KB - 212KB
-        rt_bf<1, 4, ducks::rt_layout::col> XB_col_reg; // 2KB - 214KB
-        swap_layout(XB_col_reg, XB_reg);
+        rt_bf<1, 4, ducks::rt_layout::col> &XB_col_reg = swap_layout_inplace(XB_reg);
         rt_bf<1, 16, ducks::rt_layout::col> &dl_dZ1_col_reg = swap_layout_inplace(dl_dZ1_reg);  // [K,4f]r->c TODO: tricy
         zero(delta_W1_fl_reg);
         mma_AtB(delta_W1_fl_reg, XB_col_reg, dl_dZ1_col_reg, delta_W1_fl_reg);  // ([K,f]c).t @ [K,4f]c -> [f,4f]r // 2KB - 212KB
+        swap_layout_inplace(XB_col_reg);
         copy(delta_W1_reg, delta_W1_fl_reg); // 64KB - 148KB
         rt_bf<4, 16, ducks::rt_layout::col> &delta_W1_col_reg = swap_layout_inplace(delta_W1_reg);  // TODO: tricky
 
@@ -142,7 +144,8 @@ void prefill_whole_loop_ker(
         copy(Z2_bar_term_1_reg, Z2_bar_term_1_fl_reg); // 4KB - 138.5KB
 
         zero(Z2_bar_term_2_fl_reg);
-        mma_AB(Z2_bar_term_2_fl_reg, Attn_reg, dl_dZ2_col_reg, Z2_bar_term_2_fl_reg); // 2.5KB - 140KB
+        swap_layout_inplace(dl_dZ2_reg);
+        mma_AB(Z2_bar_term_2_fl_reg, Attn_reg, dl_dZ2_col_reg, Z2_bar_term_2_fl_reg); // r,c->r 2.5KB - 140KB
         copy(Z2_bar_term_2_reg, Z2_bar_term_2_fl_reg); // 4KB - 136KB
 
         sub(Z2_bar_term_1_reg, Z2_bar_term_1_reg, Z2_bar_term_2_reg);  // cannot multiplex Z2_bar and Z2_bar_term_1_reg // 2KB - 134KB
