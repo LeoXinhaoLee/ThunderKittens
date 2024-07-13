2d6ca0d85338789bd37ca9ec15ef7244895c2d98
LeoXinhaoLee <Mc1114207918@outlook.com>
2024-07-12 20:15:15 -0700

    add M2 prefill no cumsum matrix

--- examples/ttt_mlp_prefill/tk_ttt_mlp_prefill_frontend.cpp
+++ examples/ttt_mlp_prefill/tk_ttt_mlp_prefill_frontend.cpp
@@ -54,8 +54,57 @@ extern void  ttt_mlp_prefill_fp16_ref(torch::Tensor W1,
                          stream);
 }
 
+extern void  ttt_mlp_prefill_no_cumsum_matrix_fp16(torch::Tensor W1,
+                                                   torch::Tensor W2,
+                                                   torch::Tensor b1,
+                                                   torch::Tensor b2,
+                                                   torch::Tensor ln_weight,
+                                                   torch::Tensor ln_bias,
+                                                   torch::Tensor make_last_b_matrix,
+                                                   torch::Tensor make_last_eta_1_matrix,
+                                                   torch::Tensor make_last_eta_2_matrix,
+                                                   torch::Tensor XV,
+                                                   torch::Tensor XK,
+                                                   torch::Tensor XQ,
+                                                   torch::Tensor Eta,
+                                                   torch::Tensor Output,
+                                                   cudaStream_t stream);
+
+
+extern void  ttt_mlp_prefill_no_cumsum_matrix_fp16_ref(torch::Tensor W1,
+                                                       torch::Tensor W2,
+                                                       torch::Tensor b1,
+                                                       torch::Tensor b2,
+                                                       torch::Tensor ln_weight,
+                                                       torch::Tensor ln_bias,
+                                                       torch::Tensor make_last_b_matrix,
+                                                       torch::Tensor make_last_eta_1_matrix,
+                                                       torch::Tensor make_last_eta_2_matrix,
+                                                       torch::Tensor XV,
+                                                       torch::Tensor XK,
+                                                       torch::Tensor XQ,
+                                                       torch::Tensor Eta,
+                                                       torch::Tensor Output) {
+    auto stream = at::cuda::getCurrentCUDAStream();
+    ttt_mlp_prefill_no_cumsum_matrix_fp16(W1,
+                                          W2,
+                                          b1,
+                                          b2,
+                                          ln_weight,
+                                          ln_bias,
+                                          make_last_b_matrix,
+                                          make_last_eta_1_matrix,
+                                          make_last_eta_2_matrix,
+                                          XV,
+                                          XK,
+                                          XQ,
+                                          Eta,
+                                          Output,
+                                          stream);
+}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.doc() = "TTT-MLP prefill kernel with ThunderKittens";
     m.def("ttt_mlp_prefill_fp16", &ttt_mlp_prefill_fp16_ref);
+    m.def("ttt_mlp_prefill_no_cumsum_matrix_fp16", &ttt_mlp_prefill_no_cumsum_matrix_fp16_ref);
 }
